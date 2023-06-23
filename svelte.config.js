6155aa8fcbcf704348cb65ab543d6fed8f5f0e43
TheUtkarsh8939 <utkarshchandel2012@gmail.com>
2023-06-23 11:55:52 +0530

    init

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,15 @@
-import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  preprocess: preprocess(),
-	kit: {
-		target: '#svelte',
-		adapter: adapter({
-			pages: 'build',  // path to public directory
-			assets: 'build',  // path to public directory
-			fallback: null
-		})
-	}
+export default {
+    kit: {
+        adapter: adapter({
+            // default options are shown. On some platforms
+            // these options are set automatically — see below
+            pages: 'build',
+            assets: 'build',
+            fallback: undefined,
+            precompress: false,
+            strict: true
+        })
+    }
 };
-
-export default config;
